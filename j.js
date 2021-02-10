b6d7ac83541a875a3e43be7be3354f1c32706390
Xavier2414 <xaviertomaspons03@gmail.com>
2021-02-10 22:08:50 +0100

    Monster

--- j.js
+++ j.js
@@ -1,75 +1,67 @@
-function trackKeys(keys) {
-    let down = Object.create(null);
-    function track(event) {
-      if (keys.includes(event.key)) {
-        down[event.key] = event.type == "keydown";
-        event.preventDefault();
-      }
-    }
-    window.addEventListener("keydown", track);
-    window.addEventListener("keyup", track);
-    down.unregister = () => {
-      window.removeEventListener("keydown", track);
-      window.removeEventListener("keyup", track);
-    }
-    return down;
+ var Monster= class Monster {
+  constructor(pos, speed, chase) {
+    this.pos = pos;
+    this.speed = speed;
+    this.chase = chase;
   }
 
-  function runLevel(level, Display) {
-    let display = new Display(document.body, level);
-    let state = State.start(level);
-    let ending = 1;
-    return new Promise(resolve => {
-      /* all of the game pausing logic has to happen
-        inside the promise function because when that
-        resolves the game level is over */
-      let gamePaused = false;
-      // function to determine if the game is paused or not
-      const isGamePaused = () => gamePaused;
-      /* function to alternately pause or resume the game
-        when the escape key is pressed */
-      const pauseOrResumeGame = (e) => {
-        if (e.key == 'Escape') {
-          gamePaused = !gamePaused;
-        }
-      };
-      /* wait for the game to resume
-        check resume status every 100 ms
-        once resumed, the animation function is called again */
-      const waitForResume = () => {
-        if (isGamePaused()) {
-          setTimeout(waitForResume, 100);
-        } else {
-          runAnimation(animate);
-        }
-      };
-      const animate = (time) => {
-        state = state.update(time, arrowKeys);
-        display.setState(state);
-        if (isGamePaused()) {
-          // game is paused, wait for it to resume
-          waitForResume();
-          // return false to stop the animation
-          return false;
-        } else if (state.status == "playing") {
-          return true;
-        } else if (ending > 0) {
-          ending -= time;
-          return true;
-        } else {
-          display.clear();
-          resolve(state.status);
-          // stop listening for the escape key
-          document.removeEventListener('keydown', pauseOrResumeGame);
-          // unregister control keys
-          arrowKeys.unregister();
-          return false;
-        }
-      };
-      // listen for the escape key to be pressed (to pause/resume game)
-      document.addEventListener('keydown', pauseOrResumeGame);
-      // register keys to control character
-      let arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"]);
-      runAnimation(animate);
-    });
+  get type() { return "monster"; }
+
+  static create(pos, ch) {
+    if (ch == 'm') {
+      // monster that moves back and forth
+      return new Monster(pos.plus(new Vec(0, -1)), new Vec(3, 0), false);
+    } else { // 'M'
+      // monster that chases player
+      return new Monster(pos.plus(new Vec(0, -1)), new Vec(3, 0), true);
+    }
   }
+
+  update(time, state) {
+    let newPos;
+    if (this.chase) {
+      if (state.player.pos.x < this.pos.x) {
+        this.speed = new Vec(-3, 0);
+      } else {
+        this.speed = new Vec(3, 0);
+      }
+    }
+    newPos = this.pos.plus(this.speed.times(time));
+    if (!state.level.touches(newPos, this.size, "wall")) {
+      return new Monster(newPos, this.speed, this.chase);
+    } else {
+      return new Monster(this.pos, this.speed.times(-1), this.chase);
+    }
+  }
+
+  collide(state) {
+    let player = state.player;
+    let monster = this;
+    if (monster.pos.y - player.pos.y > 1) {
+      let filtered = state.actors.filter(a => a != this);
+      return new State(state.level, filtered, state.status);
+    } else {
+      return new State(state.level, state.actors, 'lost');
+    }
+  }
+}
+
+Monster.prototype.size = new Vec(1.2, 2);
+
+levelChars["m"] = Monster;
+levelChars["M"] = Monster;
+
+runLevel(new Level(`
+..................................
+.################################.
+.#..............................#.
+.#..............................#.
+.#..............................#.
+.#...........................o..#.
+.#..@...........................#.
+.##########..............########.
+..........#..o..o..o..o..#........
+..........#..m........M..#........
+..........################........
+..................................
+`), DOMDisplay);
