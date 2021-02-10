fea1fbacee5941b3ef416186095d71d2e9bc10cc
Xavier2414 <xaviertomaspons03@gmail.com>
2021-02-10 23:30:16 +0100

    Correcion Problemas

--- 16_game/code/chapter/16_game.js
+++ 16_game/code/chapter/16_game.js
@@ -70,7 +70,7 @@ var Player = class Player {
   }
 }
 
-Player.prototype.size = new Vec(0.8, 1);
+Player.prototype.size = new Vec(1, 1);
 
 var Lava = class Lava {
   constructor(pos, speed, reset) {
@@ -368,7 +368,7 @@ function runLevel(level, Display) {
       } else {
         display.clear();
         window.removeEventListener("keydown", escHandler);
-        arrowKeys.unregister();
+        
         resolve(state.status);
         return false;
       }
@@ -404,3 +404,72 @@ async function runGame (plans, Display)
     }
 
 }
+
+
+
+var Monster=class Monster {
+  constructor(pos, speed, chase) {
+    this.pos = pos;
+    this.speed = speed;
+    this.chase = chase;
+  }
+
+  get type() { return "monster"; }
+
+  static create(pos, ch) {
+    if (ch == 'm') {
+
+      return new Monster(pos.plus(new Vec(0, -1)), new Vec(3, 0), false);
+    } else { 
+      return new Monster(pos.plus(new Vec(0, -1)), new Vec(3, 0), true);
+    }
+  }
+
+  update(time, state) {
+    let newPos;
+    if (this.chase) {
+      if (state.player.pos.x < this.pos.x) {
+        this.speed = new Vec(-3, 0);
+      } else {
+        this.speed = new Vec(3, 0);
+      }
+    }
+    newPos = this.pos.plus(this.speed.times(time));
+    if (!state.level.touches(newPos, this.size, "wall")) {
+      return new Monster(newPos, this.speed, this.chase);
+    } else {
+      return new Monster(this.pos, this.speed.times(-1), this.chase);
+    }
+  }
+
+  collide(state) {
+    let player = state.player;
+    let monster = this;
+    if (monster.pos.y - player.pos.y > 1) {
+      let filtered = state.actors.filter(a => a != this);
+      return new State(state.level, filtered, state.status);
+    } else {
+      return new State(state.level, state.actors, 'lost');
+    }
+  }
+}
+
+Monster.prototype.size = new Vec(1.2, 2);
+
+levelChars["m"] = Monster;
+levelChars["M"] = Monster;
+
+runLevel(new Level(`
+..................................
+.################################.
+.#..............................#.
+.#..............................#.
+.#..............................#.
+.#...........................o..#.
+.#..@...........................#.
+.##########..............########.
+..........#..o..o..o..o..#........
+..........#..m........M..#........
+..........################........
+..................................
+`), DOMDisplay);
